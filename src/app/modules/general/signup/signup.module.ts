d900448e5f26bea7082b66c021531e9c37daf0b5
Marjory Marquez <samaterra@hotmail.com>
2023-01-13 09:54:09 -0500

    signup update

--- src/app/modules/general/signup/signup.module.ts
+++ src/app/modules/general/signup/signup.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SignupComponent } from './signup.component';
 import { SignupRoutingModule } from './signup-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent
+  ],
   imports: [
     CommonModule,
     SignupRoutingModule
-  ]
+  ],
+  exports: [
+    SignupComponent
+  ],
+  providers: [
+
+  ],
 })
 export class SignupModule { }
