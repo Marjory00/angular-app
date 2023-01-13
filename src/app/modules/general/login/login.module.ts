db925e1f66d912c9b070c97e2388d14e2963c7aa
Marjory Marquez <samaterra@hotmail.com>
2023-01-13 09:59:59 -0500

    login update

--- src/app/modules/general/login/login.module.ts
+++ src/app/modules/general/login/login.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
-  ]
+  ],
+  exports: [
+    LoginComponent
+  ],
+  providers: [
+
+  ],
 })
 export class LoginModule { }
