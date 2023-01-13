5761586dc90698e1515fe0dae44e584dae83e74d
Marjory Marquez <samaterra@hotmail.com>
2023-01-13 09:29:58 -0500

    contact-update

--- src/app/modules/general/contact/contact.module.ts
+++ src/app/modules/general/contact/contact.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ContactComponent } from './contact.component';
 import { ContactRoutingModule } from './contact-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactComponent
+  ],
   imports: [
     CommonModule,
     ContactRoutingModule
-  ]
+  ],
+  exports: [
+    ContactComponent
+  ],
+  providers: [
+
+  ],
 })
 export class ContactModule { }
