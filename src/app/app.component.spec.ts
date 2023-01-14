de31033cf58336e4d19c43cfe000c2e7f54c0361
Marjory Marquez <samaterra@hotmail.com>
2023-01-13 20:12:01 -0500

    changed title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-react-starter'`, () => {
+  it(`should have as title 'angular-inmo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-react-starter');
+    expect(app.title).toEqual('angular-inmo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-react-starter app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-inmo app is running!');
   });
 });
