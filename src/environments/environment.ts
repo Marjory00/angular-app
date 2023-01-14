0438146511238a3f578880213271299222584a75
Marjory Marquez <samaterra@hotmail.com>
2023-01-13 20:00:26 -0500

    environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    application:
+    {
+        name: 'angular-inmo',
+        version: 'Angular 15.0.4',
+        bootstrap: 'Bootstrap 5.2.3',
+        fontawesome: 'Font Awesome 6.2.1',
+    }
+};
