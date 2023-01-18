2167bae9e552d77f98c1e840894a7fa502d00f06
Marjory Marquez <samaterra@hotmail.com>
2023-01-17 22:32:12 -0500

    login

--- src/app/modules/general/login/login.component.ts
+++ src/app/modules/general/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SeoService } from 'src/app/services/seo.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(private seoService: SeoService) {
+
+    const content = 'Login content with meta';
+    this.seoService.setMetaDescription(content);
+  }
 
 }
