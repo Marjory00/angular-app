d796eeee4116484550489cd9e977c47a7c08c2b8
Marjory Marquez <samaterra@hotmail.com>
2023-01-18 17:01:07 -0500

    environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,21 @@ export const environment = {
         version: 'Angular 15.0.4',
         bootstrap: 'Bootstrap 5.2.3',
         fontawesome: 'Font Awesome 6.2.1',
-    }
+    },
+    urlNews: './assets/params/json/mock/trailers.json',
+     /*   urlNews: 'http://localhost:5004/trailers', */
+
+     urlMovies: './assets/params/json/mock/movies.json',
+     //   urlNews: 'http://localhost:5004/trailers', 
+     config: {
+        /* SELECT ONE OF THOSE CONFIGURATIONS */
+
+        /* LOCAL JSON (NO CRUD) */
+        api: false,
+        url: './assets/params/json/crud/',
+
+        /* LOCAL REST API CRUD WITH POSTGRESQL */
+        /* api: true,
+        url: 'http://localhost:5004/', */
+  },
 };
